33373b6661400f1eb08c348685f0459708478a88
jhaslowh <jhaslowh@gmail.com>
2012-04-30 00:33:30 -0400

    ..

--- gui.py
+++ gui.py
@@ -88,7 +88,7 @@ class ButtonGroup:
     def draw(self, screen):
         for i in range(len(self.buttons)):
             self.buttons[i].draw2(screen, (self.loc[0], self.loc[1] + (self.spacing_y * i)))
-        screen.blit(self.selector, (self.loc[0] - 50, self.loc[1] + (self.spacing_y * self.selected_index)))
+        screen.blit(self.selector, (self.loc[0] - 60, self.loc[1] + (self.spacing_y * self.selected_index)))
     def add(self, button):
         self.buttons.append(button)
         self.buttons[len(self.buttons)-1].rec = self.buttons[len(self.buttons)-1].rec.move(self.loc[0], self.loc[1] + (self.spacing_y * (len(self.buttons)-1)))
